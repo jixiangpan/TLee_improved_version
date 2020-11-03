bbd9fcd59bc82d750c2f0c48dfef73342128ac6b
xji <xji@bnl.gov>
2020-11-03 18:33:38 -0500

    update

--- read_TLee_v20.C
+++ read_TLee_v20.C
@@ -22,10 +22,13 @@ using namespace std;
   usage:
   
   make clean
-  make
-  
+  make  
   ./read_TLee_v20 -f 1 -p 1
   
+  ---> README:
+  ---> Makefile: comment the line "ROOTSYS=/home/xji/data0/software/root_build", if you have your own "ROOTSYS"
+  ---> minuit2 is in the ROOT
+
 */
 
 int main(int argc, char** argv)
